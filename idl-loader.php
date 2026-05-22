54a9ce21d7a120b7e32c8799d722996e686ab59b
Carlos <carlos@Carloss-MacBook-Pro.local>
2026-05-22 11:53:01 -0400

    Chore: Migrate child theme repository to Custom Function Loader plugin

---
+++ idl-loader.php
@@ -0,0 +1,11 @@
+<?php 
+/**
+ * Plugin Name: Custom Function Loader by IDL
+ * Plugin URI: https://www.idlwebinc.com/
+ * Description: Custom Function Loader by IDL
+ * Author:  IDL Web Inc.
+ * Author URI: https://www.idlwebinc.com/
+ * Version: 1.0
+ */
+
+require_once("functions.php");
